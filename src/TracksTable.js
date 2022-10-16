054858528c54035945e37860042459e206d62a03
David Tang <687449+iamdtang@users.noreply.github.com>
2022-10-15 21:24:19 -0700

    Update

--- src/TracksTable.js
+++ src/TracksTable.js
@@ -1,32 +1,70 @@
+import { useState } from "react";
+
 export default function TracksTable({ tracks }) {
+  const [filteredTracks, setFilteredTracks] = useState(tracks);
+  const [search, setSearch] = useState("");
+
   return (
-    <table className="table table-striped table-sm">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Composer</th>
-          <th>Listens</th>
-          <th>Price</th>
-        </tr>
-      </thead>
-      <tbody>
-        {tracks.map((track) => {
-          return (
-            <tr key={track.id}>
-              <td>{track.attributes.name}</td>
-              <td>
-                {track.attributes.composer ? track.attributes.composer : "N/A"}
-              </td>
-              <td>
-                {new Intl.NumberFormat("en-US").format(
-                  track.attributes.listens
-                )}
-              </td>
-              <td>${track.attributes.price}</td>
+    <>
+      <input
+        type="search"
+        placeholder="Search for a track"
+        className="form-control my-3"
+        value={search}
+        onChange={(event) => {
+          const search = event.target.value;
+          setSearch(search);
+
+          const filteredTracks = tracks.filter((track) => {
+            return track.attributes.name
+              .toLowerCase()
+              .includes(search.toLowerCase());
+
+            // this is also acceptable
+            return (
+              track.attributes.name
+                .toLowerCase()
+                .indexOf(search.toLowerCase()) > -1
+            );
+          });
+
+          setFilteredTracks(filteredTracks);
+        }}
+      />
+      {filteredTracks.length > 0 ? (
+        <table className="table table-striped table-sm">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Composer</th>
+              <th>Listens</th>
+              <th>Price</th>
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
+          </thead>
+          <tbody>
+            {filteredTracks.map((track) => {
+              return (
+                <tr key={track.id}>
+                  <td>{track.attributes.name}</td>
+                  <td>
+                    {track.attributes.composer
+                      ? track.attributes.composer
+                      : "N/A"}
+                  </td>
+                  <td>
+                    {new Intl.NumberFormat("en-US").format(
+                      track.attributes.listens
+                    )}
+                  </td>
+                  <td>${track.attributes.price}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      ) : (
+        <p>No tracks found for "{search}".</p>
+      )}
+    </>
   );
 }
--- src/routes/Index.js
+++ src/routes/Index.js
@@ -1,34 +1,13 @@
-import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import TracksTable from "../TracksTable";
 
 export default function Index() {
   const tracks = useLoaderData();
-  const [filteredTracks, setFilteredTracks] = useState(tracks);
-  const [search, setSearch] = useState("");
 
   return (
-    <div>
+    <>
       <h1>Top 100 Tracks</h1>
-
-      <input
-        type="search"
-        placeholder="Search for a track"
-        className="form-control my-3"
-        value={search}
-        onChange={(event) => {
-          const search = event.target.value;
-          setSearch(search);
-
-          const filteredTracks = tracks.filter((track) => {
-            return track.attributes.name.indexOf(search) > -1;
-          });
-
-          setFilteredTracks(filteredTracks);
-        }}
-      />
-
-      <TracksTable tracks={filteredTracks} />
-    </div>
+      <TracksTable tracks={tracks} />
+    </>
   );
 }
